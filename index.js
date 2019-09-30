c3bc668c2190f6618f2ad231c38da553833d913e
karenjli <karenjy.li@gmail.com>
2019-09-30 15:13:05 -0500

    get endpoints completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db");
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("IT'S WORKING!");
+});
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(user => {
+      res.send(user);
+    })
+    .catch(error => {
+      res.send(error);
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  db.findById(id)
+    .then(user => {
+      res.send(user);
+    })
+    .catch(error => {
+      res.send(error);
+    });
+});
+
+const port = 5000;
+
+server.listen(port, () => {
+  console.log("API is listening");
+});
